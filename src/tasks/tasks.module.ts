4c02ca2d724f302348c7ce0672e500bab17cfc34
Rrailee <vlad107a@gmail.com>
2025-04-06 05:50:47 +0300

    taskmodule

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
-import { UsersModule } from 'users/users.module';
-import { PrismaService } from '@prisma/prisma.service';
+import { PrismaService } from 'src/prisma/prisma.service'; // путь может отличаться
+import { UsersService } from 'src/users/users.service';    // путь тоже проверь
 
 @Module({
-  imports: [UsersModule],
-  providers: [TasksService, PrismaService]
+  controllers: [TasksController],
+  providers: [TasksService, PrismaService, UsersService],
+  exports: [TasksService],
 })
 export class TasksModule {}
